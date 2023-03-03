83b8adaaf41d8c176ddf122464c6ab4b91d07943
praveenkrishna03 <gpkgpk2003@gmail.com>
2023-03-03 21:29:54 +0530

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-import 'dart:async';
-import 'package:firebase_core/firebase_core.dart';
+//import 'dart:async';
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
+  //WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
   runApp(MyApp());
 }
 
