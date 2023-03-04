5df3c352e8ae1ac1c861bb855047341a4cb03503
praveenkrishna03 <gpkgpk2003@gmail.com>
2023-03-04 14:21:11 +0530

    new file:   lib/assets/images/account.png 	new file:   lib/assets/images/for_child.png 	new file:   lib/assets/images/for_mom.png 	new file:   lib/assets/images/fun_time.png 	new file:   lib/assets/images/physical_health.png 	new file:   lib/details.dart

---
+++ lib/details.dart
@@ -0,0 +1,18 @@
+// user details
+String usr_phone = '';
+String usr_name = '';
+String usr_age = '';
+String usr_email = '';
+String usr_pmonth = '';
+String usr_height = '';
+String usr_weight = '';
+
+//doctor details
+String doc_phone = '';
+String doc_name = '';
+
+//well wisher details
+String wel_phone = '';
+String wel_name = '';
+
+bool det_gather = false;
